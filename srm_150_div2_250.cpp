f2a0c443864ae3067e0da1f0a9195b5f4245d10f
kiwi <kiwi.swhite.coder@gmail.com>
2012-04-28 17:40:52 +0800

    [kiwi] simple srm_150_div2_250

--- srm_150_div2_250.cpp
+++ srm_150_div2_250.cpp
@@ -1,37 +1,22 @@
 #include <vector>
+#include <cmath>
 using namespace std;
 
 class WidgetRepairs
 {
 public:
-	int workday(int& leave, int numPerDay)
-	{
-	 	if( ( leave < numPerDay) && (leave > 0) )
-			{
-				leave = 0;
-			}
-		else{
-		leave -= numPerDay;
-		}
-		return 1;
-	}
-
 	int days(vector<int> arrivals, int numPerDay)
 	{
-		int count = 0;
-		int initial = 0;
-		int& leave = initial;
-		for (int i = 0; i < arrivals.size(); i++ )
-		{ 
-			leave += arrivals[i];
-			if(leave == 0) continue;
-			count += workday(leave,numPerDay);
+		int widget = 0;
+		int res = 0;
+		for (int i = 0; i < arrivals.size(); i++) {
+			widget += arrivals[i];
+			if (widget > 0) res++;
+			if (widget < numPerDay) widget = 0;
+			else widget -= numPerDay;
 		}
- 		while(leave > 0)
- 		{
-			count += workday(leave,numPerDay);
- 		}
-		return count;
+		
+		return res + ceil(1.0 * widget / numPerDay);
 	}
 	
 };

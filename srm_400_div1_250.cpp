fbb0e0e89617d8e0e024152c16e836772d342fb6
kiwi <kiwi.swhite.coder@gmail.com>
2012-06-13 11:23:23 +0800

    [kiwi] srm_400_div1_250 use pow instead of binary search, see ACRush

--- srm_400_div1_250.cpp
+++ srm_400_div1_250.cpp
@@ -24,36 +24,24 @@ public:
 	vector <int> baseAndExponent(string); 
 	long long str2ll(const string &str) {
 		long long res;
-		sscanf(str.c_str(),"%lld", &res);
+		sscanf(str.c_str(), "%lld", &res);
 		return res;
-	}
-	
-	bool isPrime(long long x) {
-		for (long long i = 2; i*i <= x; i++)
-			if (x % i == 0) return false;
-		return true;
 	}
 	
 	long long solve(long long val, long long power) {
-		return subsolve(val, power, 2, 1000000000);
+		return round(pow(1.0*val, 1.0/power));
 	}
 	
-	long long subsolve(long long val, long long power, long long l, long long r) {
-		if (l > r) return -1;
-		long long mid = (l+r)/2;
-		long long tmp = power_up(mid, power);
-		if (tmp == val) return mid;
-		if (tmp == 0 || tmp > val) return subsolve(val, power, l, mid-1);
-		else return subsolve(val, power, mid+1, r);
+	bool isPrime(int x) {
+		for (int p = 2; p*p <= x; p++)
+			if (x % p == 0) return false;
+		return true;
 	}
 	
-	long long power_up(long long base, long long power) {
-		long long res = 1;
-		for (long long p = 1; p <= power; p++) {
-			res *= base;
-			if (res < 0) return 0;
-		}
-		return res;
+	bool check(long long base, long long power, long long val) {
+		long long temp = 1;
+		for (int i = 1; i <= power; i++) temp *= base;
+		return temp == val;
 	}
 };
 
@@ -62,8 +50,8 @@ vector <int> StrongPrimePower::baseAndExponent(string n) {
 		
 	for (long long power = 2; power <= 62; power ++) {
 		long long base = solve(N, power);
-		if (base < 0) continue;
 		if (!isPrime(base)) continue;
+		if (!check(base, power, N)) continue;
 		vector<int> res(2,0);
 		res[0] = base; res[1] = power;
 		return res;
@@ -72,5 +60,5 @@ vector <int> StrongPrimePower::baseAndExponent(string n) {
 	return vector<int>();
 }
 
-<%:testing-code%>
+
 //Powered by [KawigiEdit] 2.0!

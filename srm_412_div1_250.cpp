0543877db0e5fc1d959986e128a6161c1ccb197b
kiwi <kiwi.swhite.coder@gmail.com>
2012-06-17 21:30:08 +0800

    [kiwi] srm_412_div1_250 compressed state dp Petr

--- srm_412_div1_250.cpp
+++ srm_412_div1_250.cpp
@@ -22,34 +22,18 @@ using namespace std;
 class ForbiddenStrings {
 public:
 	long long countNotForbidden(int);
-	// A is 0, B is 1, C is 2
-	long long dp[35][3][3];
-	bool vis[35][3][3];
-	
-	long long solve(int L, int X, int Y) {
-		if (L <= 2) return 1;
-		
-		if (vis[L][X][Y]) return dp[L][X][Y];
-		vis[L][X][Y] = true;
-		dp[L][X][Y] = 0;
-		for (int Z = 0; Z < 3; Z++) {
-			if (X != Y && Y != Z && Z != X) continue;
-			dp[L][X][Y] += solve(L-1, Y, Z);
-		}
-		return dp[L][X][Y];
-	}
+	long long same[35];
+	long long diff[35];
 };
 
-long long ForbiddenStrings::countNotForbidden(int n) {
-	memset(vis, false, sizeof(vis));
-	if (n == 1) return 3;
-	if (n == 2) return 9;
-	
-	long long res = 0;
-	for (int i = 0; i < 3; i++)
-	for (int j = 0; j < 3; j++)
-		res += solve(n, i, j);
-	return res;
+long long ForbiddenStrings::countNotForbidden(int N) {
+	same[1] = 0; same[2] = 3;
+	diff[1] = 3; diff[2] = 6;
+	for (int n = 3; n <= N; n++) {
+		same[n] = same[n-1] + diff[n-1];
+		diff[n] = same[n-1] * 2 + diff[n-1];
+	}
+	return same[N] + diff[N];
 }
 
 <%:testing-code%>

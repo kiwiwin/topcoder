8400a23ca4aeb70da2e3118925ed7059e47699a3
kiwi <kiwi.swhite.coder@gmail.com>
2012-07-23 10:47:28 +0800

    srm_438_div1_300 extract some methods

--- srm_438_div1_300.cpp
+++ srm_438_div1_300.cpp
@@ -31,31 +31,58 @@ struct Cmp {
 class UnluckyIntervals {
 public:
 	vector<int> lucky;
+	int firstN;
+
+	vector<int> smallUnlucky() {
+		vector<int> result;
+		for (int i = 0; i < lucky.size(); i++) {
+			int cnt = 0;
+			for (int x = lucky[i]-1; x > (i>0?lucky[i-1]:0); x--) {
+				result.push_back(x);
+				cnt ++; 
+				if (cnt >= firstN) break;
+			}
+		}	
+			
+		return result;
+	}
+	
+	
+	vector<int> bigUnlucky() {
+		vector<int> result;
+	
+		for (int i = 0; i < lucky.size(); i++) {		
+			int cnt = 0;
+			for (int x = lucky[i]+1; x < (i<lucky.size()-1?lucky[i+1]:lucky[i]+firstN); x++) {
+				result.push_back(x);
+				cnt ++;
+				if (cnt >= firstN) break;
+			}
+		}
+	
+		return result;		
+	}
+	
+	vector<int> getAllPossibleValue() {
+		set<int> S;
+		S.insert(lucky.begin(), lucky.end());
+		vector<int> small = smallUnlucky();
+		S.insert(small.begin(), small.end());
+		vector<int> big = bigUnlucky();
+		S.insert(big.begin(), big.end());
+		S.erase(0);
+		
+		return vector<int>(S.begin(), S.end());
+	}
 
 	vector<int> getLuckiest(vector<int> luckySet, int n) {
+		firstN = n;
 		sort(luckySet.begin(), luckySet.end());
 		
 		lucky = vector<int>(luckySet.size()+1, 0);
 		lucky[0] = 0; copy(luckySet.begin(), luckySet.end(), lucky.begin()+1); 
 				
-		set<int> S;
-		for (int i = 0; i < lucky.size(); i++) {
-			if (lucky[i] > 0) S.insert(lucky[i]);
-			int cnt = 0;
-			for (int x = lucky[i]-1; x > (i>0?lucky[i-1]:0); x--) {
-				S.insert(x);
-				cnt ++; 
-				if (cnt >= n) break;
-			}
-			cnt = 0;
-			for (int x = lucky[i]+1; x < (i<lucky.size()-1?lucky[i+1]:lucky[i]+n); x++) {
-				S.insert(x);
-				cnt ++;
-				if (cnt >= n) break;
-			}
-		}
-		
-		vector<int> result(S.begin(), S.end());
+		vector<int> result = getAllPossibleValue();
 		Cmp cmp(lucky);
 		sort(result.begin(), result.end(), cmp);
 
